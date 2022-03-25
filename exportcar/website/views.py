6a90d12031daba0b29c94dde623abe9b5cfb6f69
SagingaliMamay <sagingali.mamayev@gmail.com>
2022-03-25 23:54:30 +0100

    pages connection is done

--- exportcar/website/views.py
+++ exportcar/website/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render (request, 'home.html', {})
+    return render(request, 'home.html', {})
+
+
+# contact page
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+# about page
+
+def about(request):
+    return render(request, 'about.html', {})
