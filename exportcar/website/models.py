c118106a730f04a691ebe2467267d2b0cf7282bd
SagingaliMamay <sagingali.mamayev@gmail.com>
2022-03-30 22:03:33 +0200

    creation model car data

--- exportcar/website/models.py
+++ exportcar/website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Car_data(models.Model):
+    mark = models.CharField(max_length=15)
+    year = models.IntegerField()
+    nrg = models.CharField(max_length=15)
+    transmission = models.CharField(max_length=15)
+    km = models.IntegerField()
+    price = models.IntegerField()
+    description = models.CharField(max_length=255)
+    email = models.EmailField(max_length=50)
+    name = models.CharField(max_length=30)
+    tel = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
+    send_time = models.DateTimeField(auto_now_add=True)
+
