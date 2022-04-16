19d8e9cd2880ccdf9e54cd42b0e1332a09c395ba
SagingaliMamay <sagingali.mamayev@gmail.com>
2022-04-16 18:10:17 +0200

    location of manage.py file is in exportcar

--- exportcar/manage.py
+++ exportcar/manage.py
@@ -4,10 +4,9 @@ import os
 import sys
 
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exportcar.be.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exportcar.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,7 +14,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
     execute_from_command_line(sys.argv)
 
 
